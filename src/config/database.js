339d86178fb9ee69e326b60fa683eb9a51810b21
cybarry <abdulbarrshonibare@yahoo.com>
2025-10-29 22:37:33 +0100

    prep for dpeloyment debug

--- src/config/database.js
+++ src/config/database.js
@@ -9,9 +9,10 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
-    logging: console.log,
+    logging: false,
   }
 );
 
